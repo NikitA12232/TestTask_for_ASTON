c22d594e646e5864a5e95c6bb8f212806dfc4257
NikitA12232 <89834171154@mail.ru>
2023-09-22 03:17:49 +0700

    add pom file and first class

--- src/main/java/TestTask/Task_Vyacheslav.java
+++ src/main/java/TestTask/Task_Vyacheslav.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 
 public class Task_Vyacheslav {
     public static void main(String[] args) {
-        // Создаем объект Scanner для ввода с клавиатуры
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введите имя: ");
         String name = scanner.nextLine();
-
-
         if (name.equalsIgnoreCase("Вячеслав")) {
             System.out.println("Привет, Вячеслав!");
         } else {
             System.out.println("Нет такого имени");
         }
-
-        // Закрываем сканнер
         scanner.close();
     }
 }
